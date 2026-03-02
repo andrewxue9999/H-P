8d5042bb8864b32bb1c99ba3186093ea4fa44238
andrewxue9999 <donutzhong2525@gmail.com>
2026-03-01 23:21:11 -0500

    Reapply "view history -> rate history to be more consistent with what the feature actually does lol"
    This reverts commit fdb530d3a94acd2cc390b97cba178eb7ee1841f6.

--- src/app/term-types/page.tsx
+++ src/app/term-types/page.tsx
@@ -601,7 +601,7 @@ export default async function TermTypesPage({ searchParams }: TermTypesPageProps
               }`}
               href={tabHref("history")}
             >
-              View History
+              Rate History
             </a>
             <a
               className={`block rounded-xl px-3 py-2 text-sm font-medium ${
